6c892d97ac6d438dffd64e1560f48068854a5d68
email <krmukeshsingh51@gmail.com>
2021-09-19 13:38:18 +0530

    added codes

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const addBtn = document.getElementById("add");
 
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+  notes.forEach((note) => {
+    addNewNote(note);
+  });
+}
+
 addBtn.addEventListener("click", () => {
   addNewNote();
 });
 
-function addNewNote() {
+function addNewNote(text = "") {
   const note = document.createElement("div");
   note.classList.add("note");
   note.innerHTML = `
@@ -19,6 +27,10 @@ function addNewNote() {
   const deleteBtn = note.querySelector(".delete");
   const main = note.querySelector(".main");
   const textArea = note.querySelector("textarea");
+
+  textArea.value = text;
+  main.innerHTML = marked(text);
+
   editBtn.addEventListener("click", () => {
     main.classList.toggle("hidden");
     textArea.classList.toggle("hidden");
@@ -26,12 +38,25 @@ function addNewNote() {
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
+    updateLS();
   });
 
   textArea.addEventListener("input", (e) => {
     const { value } = e.target;
     main.innerHTML = marked(value);
+
+    updateLS();
   });
 
   document.body.appendChild(note);
+}
+function updateLS() {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+
+  notesText.forEach((note) => {
+    notes.push(note.value);
+  });
+
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
