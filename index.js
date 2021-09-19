702eba407c24ea72331a967743c8c286ec92af5d
email <krmukeshsingh51@gmail.com>
2021-09-19 13:01:51 +0530

    added codes

---
+++ index.js
@@ -0,0 +1,37 @@
+const addBtn = document.getElementById("add");
+
+addBtn.addEventListener("click", () => {
+  addNewNote();
+});
+
+function addNewNote() {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
+        <div class="tools">
+        <button class="edit"><i class="fa fa-edit"></i></button>
+        <button class="delete"><i class="fa fa-trash"></i></button>
+        </div>
+        <div class="main hidden"></div>
+        <textarea></textarea>
+`;
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
+  });
+
+  document.body.appendChild(note);
+}
